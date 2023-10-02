8046abe8e0154c6d7720a638508effb91c618532
cbrewer <144485839+VWbrewer@users.noreply.github.com>
2023-10-02 12:11:21 -0500

    adding chars to the code for the contents of the password

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+var chars = {
+  numbers: "0123456789"
+  lowercase = "abcdefghijklmnopqrstuvwxyz";
+  uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  symbols = "!#$%&()*+-/<=>?@^~";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+    var passwordLength = 15;
+}   
+  
+
+    for(var i=0; i <= passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber);
+    }
 
   passwordText.value = password;
 
-}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
